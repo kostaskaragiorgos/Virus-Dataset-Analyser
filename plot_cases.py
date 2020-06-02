d9790c14d2feb1e8f4d6ff4ef6efedbec582fe72
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2020-06-02 10:51:21 +0300

    Update plot_cases.py

--- plot_cases.py
+++ plot_cases.py
@@ -1,2 +1,8 @@
 def plot_cases(dataframe, country):
     dataframe[dataframe['Country/Region']==country].plot(x='Date', y=['Confirmed','Deaths','Recovered'])
+
+
+def cases_difference(dataframe, country,f_date,to_date,state):
+    number_of_deaths_f = dataframe.loc[(dataframe['Country/Region']==country) &(dataframe['Date']==f_date)][state]
+    number_of_deaths_t = dataframe.loc[(dataframe['Country/Region']==country) &(dataframe['Date']==to_date)][state]
+    return "Number of "+state+" from "+f_date+" to "+to_date+" in "+country+" "+str( abs(int(number_of_deaths_f)-int(number_of_deaths_t)))
