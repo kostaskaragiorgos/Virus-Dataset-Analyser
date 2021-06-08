7a234d61c7241e62884ccb5f13e43080524bfeab
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2021-06-08 13:02:12 +0300

    Update virus_dataset_analyser.py

--- virus_dataset_analyser.py
+++ virus_dataset_analyser.py
@@ -102,7 +102,7 @@ class VirusDatasetAnalyser():
         self.master.bind('<Alt-s>', lambda event: self.showinfdiff())
         self.master.bind('<Control-q>', lambda event: self.splot("deaths"))
         self.master.bind('<Control-w>', lambda event: self.splot("confirmed"))
-        
+        self.master.bind('<Alt-q>', lambda event: self.splot("recovered"))
 
 
 
