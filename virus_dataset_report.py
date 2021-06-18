cf706d6076d4fe570e200e98edc29ea61c48498f
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2021-06-18 12:02:20 +0300

    Create virus_dataset_report.py

---
+++ virus_dataset_report.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def createdataframe(filename):
+    """
+    creates a dataframe.
+    Args:
+        filename: a csv file
+    Returns:
+        a dataframe
+    """
+    return pd.read_csv(filename)
